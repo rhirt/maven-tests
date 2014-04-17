bd5729f354b1cf0b153d8360242e241da3f19521
admin <admin@camaro>
2014-04-17 22:24:00 +0200

    initial commit

---
+++ App02/src/main/java/ch/srsx/app/App02.java
@@ -0,0 +1,10 @@
+package ch.srsx.app;
+
+public class App02 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Constructor of App02");
+    }
+
+}
