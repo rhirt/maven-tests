8bfd6e83ab7ee4efbbe18d3bddab59b496a31eb1
rhirt <rhirt@schlagundrahm.ch>
2014-09-11 21:50:28 +0200

    pom

--- App01/src/main/java/ch/srsx/app/App01.java
+++ App01/src/main/java/ch/srsx/app/App01.java
@@ -1,10 +1,18 @@
 package ch.srsx.app;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class App01 {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         System.out.println("Constructor of App01");
+        
+        Pattern p = Pattern.compile("^(?!\\s*$).+");
+        Matcher m = p.matcher("1.5.8");
+        boolean b = m.matches();
+        System.out.println("Match: " + b);
     }
 
 }
